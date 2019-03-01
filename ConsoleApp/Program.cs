3896df2a9ebfbcc1cf79a54b2fcf4cc58f626bd6
Unknown <evangelion0128@gmail.com>
2019-03-01 21:06:18 +0800

    Add base socket active
    - Use async way to active

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Asterius;
+using Asterius.Base;
+using System;
+
+namespace ConsoleApp
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Service a = new Service(
+                "127.0.0.1",
+                8080
+            );
+
+            Clew.Info("QAQ");
+        }
+    }
+}
